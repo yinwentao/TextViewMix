e0c1a826f908375557cba5bf801d90bf67c66f9b
yinwentao <1376836598@qq.com>
2015-11-02 15:32:53 +0800

    Initial Commit

---
+++ TextViewMix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextViewMix
+//
+//  Created by 尹文涛 on 15/11/2.
+//  Copyright (c) 2015年 文涛工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextViewMix/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TextViewMix
+//
+//  Created by 尹文涛 on 15/11/2.
+//  Copyright (c) 2015年 文涛工作室. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
